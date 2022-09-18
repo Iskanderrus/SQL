394b5f4ab71a041161c92f0ff55e39792c5d2a20
Alexander Chasovskoy <a.n.chasovskoy@gmail.com>
2022-09-18 18:56:42 +0300

    Add files via upload

--- DQL/003_Aggregate Functions.sql
+++ DQL/003_Aggregate Functions.sql
@@ -1,12 +1,14 @@
+-- count total number of employees, number of unique names and family names
 SELECT
 COUNT(first_name) AS "Employee count",
 COUNT(DISTINCT first_name) AS "All unique Employee Names", 
 COUNT(DISTINCT last_name) AS "All unique Employee Famlily Names"
 FROM "public"."employees"; 
 
+-- returns formatted results for total amount of salaries, average, max and min salaries
 SELECT
 to_char(sum(salary), '999G999G999G999D99') AS "Total Salaries Paid",
-to_char(avg(salary), '99G999D99') AS "Average salary", 
+to_char(avg(salary), '999G999D99') AS "Average salary", 
 to_char(max(salary), '999G999D99') AS "Highest salary",
-to_char(min(salary), '99G999D99') AS "Lowest salary"
+to_char(min(salary), '999G999D99') AS "Lowest salary"
 FROM "public"."salaries"; 
