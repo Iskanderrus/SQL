c20e05e30804deaef66a5fae2ce754b9ddd42627
Alexander Chasovskoy <a.n.chasovskoy@gmail.com>
2022-09-18 20:34:24 +0300

    Filtering data with several conditions

--- DQL/005-Filtering with many conditions.sql
+++ DQL/005-Filtering with many conditions.sql
@@ -1,5 +1,5 @@
 /*
-filtering out IDs for all female customers under 45 years from states Oregon and New York
+filtering out IDs for all female customers under 45 years from states Oregon and New York with income over 50K
 */
 SELECT 
 customerid AS "ID", 
@@ -10,5 +10,5 @@ FROM "public"."customers"
 WHERE "public"."customers"."gender" = 'F' AND
 ("public"."customers"."state" = 'OR' OR "public"."customers"."state" = 'NY') 
 GROUP BY customerid
-HAVING NOT "public"."customers"."age" > 45
+HAVING (NOT "public"."customers"."age" > 45) AND (income > 50000)
 ORDER BY "public"."customers"."state"; 
